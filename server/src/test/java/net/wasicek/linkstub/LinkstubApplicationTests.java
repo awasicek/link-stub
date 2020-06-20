af7ef3e2e5cc4f06c13ecb6eab2cf30d2254c239
Andrew Wasicek <andrew.wasicek@gmail.com>
2020-06-20 16:04:26 -0700

    foundation for the server with spring boot

---
+++ server/src/test/java/net/wasicek/linkstub/LinkstubApplicationTests.java
@@ -0,0 +1,13 @@
+package net.wasicek.linkstub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkstubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
